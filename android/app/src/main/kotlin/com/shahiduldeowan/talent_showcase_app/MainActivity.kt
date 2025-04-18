62ce376aeff9c856cefde9dc5b0e2db58d9cc36e
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-18 12:51:40 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/shahiduldeowan/talent_showcase_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shahiduldeowan.talent_showcase_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:talent_showcase_app/app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
