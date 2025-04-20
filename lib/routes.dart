16f6b290d5773ec771685bbd1aa5811c280cf136
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-20 17:28:56 +0600

    Refactor AppRoutes class to use instance variables and apply @singleton annotation for improved dependency management

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 import 'package:talent_showcase_app/features/auth/presentation/pages/login_page.dart';
 import 'package:talent_showcase_app/features/home/presentation/pages/home_page.dart';
 import 'package:talent_showcase_app/features/splash/presentation/pages/splash_page.dart';
 
 import 'core/core_exports.dart' show RouteNames;
 
+@singleton
 class AppRoutes {
-  static final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+  final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
     RouteNames.initialPageRoute: (_) => const SplashPage(),
     RouteNames.loginPageRoute: (_) => const LoginPage(),
     RouteNames.homePageRoute: (_) => const HomePage(),
   };
 
-  static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+  Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     final WidgetBuilder? builder = routes[settings.name];
     if (builder != null) {
       return MaterialPageRoute<Widget>(builder: builder, settings: settings);
