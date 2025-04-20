b05e6dedf8eaa1dd0cf5e9dd6d952dbf2fd02552
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-20 17:29:17 +0600

    Add core_exports.dart to centralize exports for application modules

---
+++ lib/core/core_exports.dart
@@ -0,0 +1,7 @@
+export 'constants/app_constants.dart';
+export 'constants/route_names.dart';
+export 'constants/asset_paths.dart';
+export 'config/app_config.dart';
+export 'theme/my_theme.dart';
+export 'theme/theme_extensions.dart';
+export 'services/navigation_service.dart';
