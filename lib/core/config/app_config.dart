97d4900d0fe8197aaaf45c41de829f9f173f74f8
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-20 17:29:09 +0600

    Add AppConfig class for application configuration settings

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  static const String appName = 'Talent Showcase App';
+  static const bool debugShowCheckedModeBanner = false;
+  static const ThemeMode themeMode = ThemeMode.light;
+}
