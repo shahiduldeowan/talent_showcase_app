02773dbd6fdd622cc8517986c0e78dbb8d8bcff9
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-22 01:59:59 +0600

    Add missing exports for app colors, gradients, text field, gradient text, and validators

--- lib/core/core_exports.dart
+++ lib/core/core_exports.dart
@@ -4,7 +4,9 @@ export 'constants/route_names.dart';
 export 'constants/asset_paths.dart';
 export 'config/app_config.dart';
 export 'theme/my_theme.dart';
+export 'theme/app_colors.dart';
 export 'theme/theme_extensions.dart';
+export 'theme/gradients.dart';
 export 'services/navigation_service.dart';
 export 'errors/exceptions.dart';
 export 'errors/failures.dart';
@@ -13,6 +15,9 @@ export 'utils/use_case.dart';
 export 'utils/type_defs.dart';
 export 'utils/flushbar_utils.dart';
 export 'utils/app_logger.dart' show logger, AppLogger;
+export 'widgets/app_text_field.dart';
+export 'widgets/gradient_text.dart';
+export 'utils/validators.dart';
 
 // 3rd party packages
 export 'package:dartz/dartz.dart';
