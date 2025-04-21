af5f19507c9458aa9e8607c8ac7b09e2f21cdf5c
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-21 11:18:14 +0600

    Update UseCase class to use FutureResult for improved asynchronous handling

--- lib/core/utils/use_case.dart
+++ lib/core/utils/use_case.dart
@@ -1,3 +1,13 @@
+import 'package:talent_showcase_app/core/utils/type_defs.dart'
+    show FutureResult;
+
+/// An abstract class representing a use case in the application.
+///
+/// [T] is the return type of the use case.
+/// [P] is the type of the parameter passed to the use case.
 abstract class UseCase<T, P> {
-  Future<T> call({P param});
+  /// Executes the use case with the given [param].
+  ///
+  /// [param] is optional and can be null for use cases that do not require parameters.
+  FutureResult<T> call({P? param});
 }
