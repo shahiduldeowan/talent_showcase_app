d4265d43c3c6bda5e6a9986fda5b3b8cc36cb698
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-21 18:45:41 +0600

    Refactor app.dart to use dependency injection for theme, routing, and navigation services

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:talent_showcase_app/core/core_exports.dart'
-    show AppConfig, MyTheme, NavigationService, RouteNames;
-import 'package:talent_showcase_app/core/di/di.dart' show getIt;
+    show AppConfig, MyTheme, NavigationService, RouteNames, getIt;
+
 import 'package:talent_showcase_app/routes.dart';
 
 class App extends StatelessWidget {
@@ -9,14 +9,18 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MyTheme myTheme = getIt<MyTheme>();
+    final AppRoutes appRouter = getIt<AppRoutes>();
+    final NavigationService navigationService = getIt<NavigationService>();
+
     return MaterialApp(
       title: AppConfig.appName,
       debugShowCheckedModeBanner: AppConfig.debugShowCheckedModeBanner,
       themeMode: AppConfig.themeMode,
-      theme: getIt<MyTheme>().getTheme,
+      theme: myTheme.getTheme,
       initialRoute: RouteNames.initialPageRoute,
-      onGenerateRoute: getIt<AppRoutes>().onGenerateRoute,
-      navigatorKey: getIt<NavigationService>().navigatorKey,
+      onGenerateRoute: appRouter.onGenerateRoute,
+      navigatorKey: navigationService.navigatorKey,
     );
   }
 }
