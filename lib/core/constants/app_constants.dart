2cb3a25dc4becf258258b288e772072269c5ef4e
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-22 02:00:31 +0600

    Add app constants for user interface strings

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,13 @@
 class AppConstants {
+  static const String showYourSkillsToTheWorld =
+      'Show your skills to the world';
+  static const String signIn = 'Sign In';
+  static const String createAccount = 'Create Account';
+  static const String newHere = 'New Here?';
+  static const String email = 'Email';
+  static const String password = 'Password';
+
+  // App Keys
   static const String accessTokenKey = 'ACCESS_TOKEN_KEY';
   static const String refreshTokenKey = 'REFRESH_TOKEN_KEY';
 }
