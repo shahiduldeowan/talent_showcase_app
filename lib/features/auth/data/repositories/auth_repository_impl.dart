c1bf4807e7bf33c7c7721543aac517711bad2fa8
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-22 20:44:14 +0600

    Add login method to AuthRepository and implement in AuthRepositoryImpl for authentication handling

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -2,13 +2,21 @@ import 'package:injectable/injectable.dart';
 import 'package:talent_showcase_app/core/core_exports.dart'
     show ErrorHandler, Failure, FailureExtensions, FutureResult, Left, Right;
 import 'package:talent_showcase_app/features/auth/data/datasources/auth_local_data_source.dart';
+import 'package:talent_showcase_app/features/auth/data/datasources/unauth_remote_data_source.dart';
+import 'package:talent_showcase_app/features/auth/data/models/auth_model.dart';
+import 'package:talent_showcase_app/features/auth/domain/entities/auth_entity.dart';
+import 'package:talent_showcase_app/features/auth/domain/entities/auth_request_entity.dart';
 import 'package:talent_showcase_app/features/auth/domain/repositories/auth_repository.dart';
 
 @LazySingleton(as: AuthRepository)
 class AuthRepositoryImpl implements AuthRepository {
-  AuthRepositoryImpl({required this.authLocalDataSource});
+  AuthRepositoryImpl({
+    required this.authLocalDataSource,
+    required this.unauthRemoteDataSource,
+  });
 
   final AuthLocalDataSource authLocalDataSource;
+  final UnauthRemoteDataSource unauthRemoteDataSource;
 
   @override
   FutureResult<bool> isAuthenticated() async {
@@ -32,5 +40,36 @@ class AuthRepositoryImpl implements AuthRepository {
       failure.logError();
       return Left<Failure, void>(failure);
     }
+  }
+
+  @override
+  FutureResult<AuthEntity?> login(AuthRequestEntity request) async {
+    try {
+      final AuthResponseModel result = await unauthRemoteDataSource.login(
+        request.toModel(),
+      );
+      AuthEntity? entity = result.data?.toEntity();
+      if (_isAliveAccessToken(entity)) _cacheAuthData(entity!);
+
+      return Right<Failure, AuthEntity?>(entity);
+    } catch (e) {
+      final Failure failure = ErrorHandler.handleError(e);
+      failure.logError();
+
+      return Left<Failure, AuthEntity>(failure);
+    }
+  }
+
+  /// Checks if the access token in the entity is valid.
+  bool _isAliveAccessToken(AuthEntity? entity) {
+    return entity?.accessToken?.isNotEmpty ?? false;
+  }
+
+  /// Caches authentication data locally.
+  Future<void> _cacheAuthData(AuthEntity entity) async {
+    await authLocalDataSource.cacheAuthData(
+      accessToken: entity.accessToken!,
+      refreshToken: entity.refreshToken ?? '',
+    );
   }
 }
--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,6 +1,9 @@
 import 'package:talent_showcase_app/core/core_exports.dart' show FutureResult;
+import 'package:talent_showcase_app/features/auth/domain/entities/auth_entity.dart';
+import 'package:talent_showcase_app/features/auth/domain/entities/auth_request_entity.dart';
 
 abstract class AuthRepository {
   FutureResult<bool> isAuthenticated();
   FutureResult<void> logout();
+  FutureResult<AuthEntity?> login(AuthRequestEntity request);
 }
