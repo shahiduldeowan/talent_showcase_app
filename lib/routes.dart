8a1a87c1e73faaac4a90b96c96a37e3459e36fd3
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-22 14:27:10 +0600

    Replace HomePage route with MainShell route in AppRoutes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:talent_showcase_app/features/auth/presentation/pages/login_page.dart';
-import 'package:talent_showcase_app/features/home/presentation/pages/home_page.dart';
+import 'package:talent_showcase_app/features/main/presentation/pages/main_shell.dart';
 import 'package:talent_showcase_app/features/splash/presentation/pages/splash_page.dart';
 
 import 'core/core_exports.dart' show RouteNames;
@@ -11,7 +11,7 @@ class AppRoutes {
   final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
     RouteNames.initialPageRoute: (_) => const SplashPage(),
     RouteNames.loginPageRoute: (_) => const LoginPage(),
-    RouteNames.homePageRoute: (_) => const HomePage(),
+    RouteNames.mainShellRoute: (_) => const MainShell(),
   };
 
   Route<dynamic>? onGenerateRoute(RouteSettings settings) {
