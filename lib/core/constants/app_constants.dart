0f0a0224da2585f48cfb9910b0f035932a380d91
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-22 18:25:48 +0600

    Add additional app constants for user authentication and account creation messages

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -6,6 +6,11 @@ class AppConstants {
   static const String newHere = 'New Here?';
   static const String email = 'Email';
   static const String password = 'Password';
+  static const String pleaseEnterAValidEmail = 'Please enter a valid email';
+  static const String passwordMustBe6Characters =
+      'Password must be at least 6 characters';
+  static const String createAccountMessage =
+      'Create account page will be soon...';
 
   // App Keys
   static const String accessTokenKey = 'ACCESS_TOKEN_KEY';
