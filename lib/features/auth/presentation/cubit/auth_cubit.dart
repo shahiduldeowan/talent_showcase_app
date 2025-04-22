34ade55a8d185455c1e34ef3eca8363c9b67c6b3
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-22 14:28:24 +0600

    Add AuthCubit and AuthState classes for authentication management

---
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:talent_showcase_app/core/core_exports.dart'
+    show EmailFormz, PasswordFormz;
+import 'package:formz/formz.dart';
+
+part 'auth_state.dart';
+part 'auth_cubit.freezed.dart';
+
+@injectable
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(const AuthState());
+}
---
+++ lib/features/auth/presentation/cubit/auth_state.dart
@@ -0,0 +1,11 @@
+part of 'auth_cubit.dart';
+
+@freezed
+abstract class AuthState with _$AuthState {
+  const factory AuthState({
+    @Default(EmailFormz.pure()) EmailFormz email,
+    @Default(PasswordFormz.pure()) PasswordFormz password,
+    @Default(FormzSubmissionStatus.initial) FormzSubmissionStatus status,
+    @Default('') String errorMessage,
+  }) = _AuthState;
+}
