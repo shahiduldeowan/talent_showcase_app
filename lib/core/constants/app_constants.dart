4172027f04e942e7bb643d3038258323413bb4fd
Shahidul Deowan <deowanshahidul@gmail.com>
2025-04-23 18:06:29 +0600

    feat: add new constants for dialogs and buttons
    - add constants for dialog messages (e.g., "are you sure?")
    - add constants for button texts (e.g., "sign in", "create account")

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,8 +1,6 @@
 class AppConstants {
   static const String showYourSkillsToTheWorld =
       'Show your skills to the world';
-  static const String signIn = 'Sign In';
-  static const String createAccount = 'Create Account';
   static const String newHere = 'New Here?';
   static const String email = 'Email';
   static const String password = 'Password';
@@ -11,8 +9,22 @@ class AppConstants {
       'Password must be at least 6 characters';
   static const String createAccountMessage =
       'Create account page will be soon...';
-  static const String invalidCredentialsMessage = 'Email and password is doesn\'t match!';
+  static const String invalidCredentialsMessage =
+      'Email and password is doesn\'t match!';
   static const String loginSuccess = 'Login Success!';
+
+  // Dialog message
+  static const String areYourSure = 'Are you sure?';
+  static const String areYouSureYouWantToExit =
+      'Are you sure you want to exit?';
+
+  // Button text
+  static const String createAccount = 'Create Account';
+  static const String signIn = 'Sign In';
+  static const String yes = 'Yes';
+  static const String no = 'No';
+  static const String home = 'Home';
+  static const String profile = 'Profile';
 
   // App Keys
   static const String accessTokenKey = 'ACCESS_TOKEN_KEY';
